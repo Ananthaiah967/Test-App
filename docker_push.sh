d90a119c580036fd14b1f888167f91af5b10f288
Ananthaiah967 <118709246+Ananthaiah967@users.noreply.github.com>
2022-11-23 00:35:27 +0530

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,6 @@
 cd /var/lib/jenkins/workspace/Test_App_Deployment/Application/v2/
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 237040306463.dkr.ecr.eu-central-1.amazonaws.com
-docker build -t test .
-docker tag test:latest 237040306463.dkr.ecr.eu-central-1.amazonaws.com/test:latest
-docker push 237040306463.dkr.ecr.eu-central-1.amazonaws.com/test:latest
+sudo docker build -t test .
+sudo docker tag test:latest 237040306463.dkr.ecr.eu-central-1.amazonaws.com/test:latest
+sudo docker push 237040306463.dkr.ecr.eu-central-1.amazonaws.com/test:latest
