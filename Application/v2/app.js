79f843e631fee858aa394fb5f07cac5ba686ee48
Ananthaiah967 <118709246+Ananthaiah967@users.noreply.github.com>
2022-11-24 13:55:21 +0530

    Update app.js

--- Application/v2/app.js
+++ Application/v2/app.js
@@ -10,7 +10,7 @@ app.use(express.static('static'));
 app.use(morgan('combined'));
 
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "This is my First Kubernetes Project!";
+var message = process.env.MESSAGE || "This is my Kubernetes Project!";
 
 app.get('/', function (req, res) {
     res.render('home', {
